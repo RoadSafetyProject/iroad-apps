439c2b64ac5b360614b2ebf7d9984870343aa0ab
Joseph chingalo <profschingalo@gmail.com>
2015-08-07 15:46:49 +0300

    enable closing dialog for adding new facility

--- community-police/controllers/appController.js
+++ community-police/controllers/appController.js
@@ -198,6 +198,13 @@ var eventCaptureControllers = angular.module('eventCaptureControllers', ['uiGmap
     		
     		
     	}
+
+		//function to close adding facility dialogue
+		$scope.cancelMenu = function(){
+
+			$scope.isAddingFacility = false;
+		}
+
     	var baseOptions = {
                 'maxZoom': 15,
                 'minZoom': 4,
