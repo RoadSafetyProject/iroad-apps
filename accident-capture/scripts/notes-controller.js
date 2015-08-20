e9fa0acdc29f332bbf8a47898dfeb69fec354781
Joseph chingalo <profschingalo@gmail.com>
2015-08-20 11:56:06 +0300

    add hide and show button for passengers forms during editing

--- accident-capture/scripts/notes-controller.js
+++ accident-capture/scripts/notes-controller.js
@@ -16,7 +16,6 @@ eventCaptureControllers.controller('AccidentController',
 
 		//fetching accident vehicle based on accident id
 		$scope.accidentVehicleEventModal.get({value:$scope.accident_id},function(result){
-			//console.log('accident vehicles data : ' + JSON.stringify(result));
 			console.log('Loading vehicles');
 			$scope.accidentVehicles = result;
 
@@ -30,9 +29,7 @@ eventCaptureControllers.controller('AccidentController',
 					}
 				);
 			}
-
 			$scope.accidentPasengerVisibility = passangerVisibity;
-			console.log(JSON.stringify($scope.accidentPasengerVisibility));
 			passangerVisibity = [];
 			$scope.loadAccidentVihecles = false;
 			$scope.$apply();
@@ -42,7 +39,6 @@ eventCaptureControllers.controller('AccidentController',
 		$scope.accidentWitnessEventModel = new iroad2.data.Modal('Accident Witness',[]);
 
 		$scope.accidentWitnessEventModel.get(new iroad2.data.SearchCriteria('Program_Accident',"=",$scope.accident_id),function(results){
-			//console.log('Witness on accident : ' + JSON.stringify(results));
 			console.log('Loading witness');
 			$scope.accidentWitnesses = results;
 			$scope.loadAccidentWitnesses = false;
@@ -54,7 +50,6 @@ eventCaptureControllers.controller('AccidentController',
 		$scope.accidentPassengersEvent = new iroad2.data.Modal('Accident Passenger',[]);
 
 		$scope.accidentPassengersEvent.get(new iroad2.data.SearchCriteria('Program_Accident',"=",$scope.accident_id),function(results){
-			console.log('Passengers : ' + JSON.stringify(results));
 			console.log('Loading accidentPassengers');
 			$scope.accidentPassengers = results;
 			$scope.loadAccidentPasssenger = false;
@@ -64,8 +59,6 @@ eventCaptureControllers.controller('AccidentController',
 
 		//function to hide or show passengers
 		$scope.showHideAccidentPassengers = function(vehicle){
-
-			console.log('hide/show vehicle : ' + vehicle);
 			for(var i = 0; i < $scope.accidentPasengerVisibility.length;i++ ){
 				if( $scope.accidentPasengerVisibility[i].vehicle == vehicle){
 					$scope.accidentPasengerVisibility[i].visibility = !$scope.accidentPasengerVisibility[i].visibility;
@@ -136,7 +129,6 @@ eventCaptureControllers.controller('EditAccidentController',function($scope,$htt
 	var accidentWitness = [];
 	$scope.accidentWitnessEventModel = new iroad2.data.Modal('Accident Witness',[]);
 	$scope.accidentWitnessEventModel.get(new iroad2.data.SearchCriteria('Program_Accident',"=",$scope.accident_id),function(results){
-		//console.log('Witness on accident : ' + JSON.stringify(results));
 		console.log('Loading witness');
 		$scope.editedaccidentWitnesses = results;
 		$scope.loadAccidentWitnesses = false;for(var i = 0; i < $scope.editedaccidentWitnesses.length; i++){
@@ -152,7 +144,6 @@ eventCaptureControllers.controller('EditAccidentController',function($scope,$htt
 	$scope.accidentPassengersEvent = new iroad2.data.Modal('Accident Passenger',[]);
 
 	$scope.accidentPassengersEvent.get(new iroad2.data.SearchCriteria('Program_Accident',"=",$scope.accident_id),function(results){
-		console.log('Passengers : ' + JSON.stringify(results));
 		console.log('Loading accidentPassengers');
 		var list = [];
 		$scope.editedAccidentPassengers = results;
@@ -173,8 +164,6 @@ eventCaptureControllers.controller('EditAccidentController',function($scope,$htt
 
 	//function to hide or show passengers
 	$scope.showHideAccidentPassengers = function(vehicle){
-
-		console.log('hide/show vehicle : ' + vehicle);
 		for(var i = 0; i < $scope.accidentPasengerVisibility.length;i++ ){
 			if( $scope.accidentPasengerVisibility[i].vehicle == vehicle){
 				$scope.accidentPasengerVisibility[i].visibility = !$scope.accidentPasengerVisibility[i].visibility;
@@ -247,12 +236,9 @@ eventCaptureControllers.controller('EditAccidentController',function($scope,$htt
 
 		for (var i=0; i < $scope.editedAccidentVehicles.length; i++ ) {
 			var licenceNumber = $scope.editedAccidentVehicles[i]['Licence Number'];
-
 			$scope.driver = null;
 			$scope.accidentVehicle = $scope.editedAccidentVehicles[i];
-
 			$scope.driverModel =  new iroad2.data.Modal('Driver',[]);
-
 			$scope.driverModel.get({value:licenceNumber},function(result){
 
 				if($scope.driver == result[0]){
@@ -473,8 +459,6 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 
 	}
 
-
-
 	//function to provide form for adding accident vehicle info
 	var numberOfVehicle = [];
 	var numberOfPassengers = []
@@ -487,7 +471,6 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 		$scope.accidentAttendant = $scope.otherDataForm['accidentAttendant'];
 
 		//$scope.accidentBasicInfoVisibility = !$scope.accidentBasicInfoVisibility;
-
 
 		for (var i=0; i < $scope.numberOfVehicles; i++ ){
 			numberOfVehicle.push(i);
@@ -503,7 +486,6 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 				}
 			);
 			numberOfPassengers.push(passengerInfo);
-
 		}
 		$scope.passengerFormsVisibility = formsVisibility;
 		$scope.newAccidentVehiclePassengers = numberOfPassengers;
@@ -662,7 +644,6 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 
 												//loop passengers data
 												for(var passengerCounter = 0; passengerCounter < $scope.newAccidentVehiclePassengers[i].counter.length; passengerCounter ++){
-
 													//prepare data for saving
 													var passenger = accidentPassengers[passengerCounter];
 													passenger.Vehicle = vehicle;
@@ -678,7 +659,6 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 														console.log('Fail to add accident passenger');
 
 													},$scope.accidentPassengerEvent.getModalName());
-
 												}
 
 											}
@@ -689,7 +669,6 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 												//add vehicle and driver objects ready for saving
 												$scope.accidentVehicle.Vehicle = vehicles[i];
 												$scope.accidentVehicle.Driver = drivers[i];
-
 
 												//add other data for driver
 												$scope.accidentVehicle['Full Name'] = $scope.accidentVehicle.Driver['Full Name'];
@@ -708,13 +687,10 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 												//add accident object
 												$scope.accidentVehicle.Accident = $scope.newAccidentForm;
 
-
 												//saving accident
 												$scope.accidentVehicleEventModal = new iroad2.data.Modal('Accident Vehicle',[]);
-
 												var savedAccidentVehicle = $scope.accidentVehicle;
 												$scope.accidentVehicleEventModal.save(savedAccidentVehicle,otherData,function(resultSavingAccidentVehicle){
-
 													console.log('Successful add accident Vehicle ' + resultSavingAccidentVehicle.importSummaries[0].reference );
 
 												},function(error){
@@ -740,311 +716,5 @@ eventCaptureControllers.controller('AddAccidentController',function($scope,$http
 		},$scope.accidentEventModal.getModalName());
 	}
 
-
-
-
-
-
 }) ;
 
-
-/*
- eventCaptureControllers.controller('offenceFormController',
- function($scope) {
- //console.log(JSON.stringify($scope));
- $scope.offence = angular.element("#offenceScope").scope().offence;
- console.log(JSON.stringify($scope.offence));
- //Initialize Offence as mirrored in the database
- var offenceModal = new iroad2.data.Modal("Offence",[new iroad2.data.Relation("Offence Registry")])
- offenceModal.get(new iroad2.data.SearchCriteria("","=",$scope.offence.id),function(results){
- console.log("Registries:"+JSON.stringify(results));
- var registries = [];
- angular.forEach(results,function(registry){
- registries.push(registry.Offence_Registry);
- });
- $scope.offenceEvents = registries;
- $scope.$apply();
- });
- //Initialize offence events of an offence
- $scope.offenceEvents = [];
- //Initialize the amount payable for the offence events
- $scope.amountPayable = 0;
-
- */
-/**
- * Updates the amount payable by calculating the sum of the amounts
- * in offenceEvents
- *//*
-
- $scope.updateAmountPayable = function(){
- $scope.amountPayable = 0;
- //Loop through the events to get amount
- for(i = 0;i < $scope.offenceEvents.length;i++)
- {
- $scope.amountPayable += parseInt($scope.offenceEvents[i].Amount);
- }
- }
- //Watch/wait for changes on offenceEvents and update the amount payable
- $scope.$watch('offenceEvents', function (newValue, oldValue) {
- $scope.updateAmountPayable();
- });
- */
-/**
- * Delete offence from from the list of offenceEvents
- *
- * @param Object(Offence) offence
- *//*
-
- $scope.deleteOffence = function(offence){
- //Loop through the offenceEvents
- for(i = 0; i < $scope.offenceEvents.length;i++)
- {
- if($scope.offenceEvents[i].id == offence.id)//If event is in the list delete it
- {
- //Delete offence from offenceEvent
- $scope.offenceEvents.splice(i,1);
- break;
- }
- }
- };
- */
-/**
- * Watch and wait for a model change and fetch from a url and execute
- * on success
- *
- * @param string model
- *
- * @param string url(Url to fetch the data)
- *
- * @param function success (To execute after success fetch)
- *//*
-
- $scope.watchAndFetch = function(model,url,success){
- $scope.$watch(model, function (value, oldValue) {
-
- if(value != '')//If the changed value is not empty
- {
- //Fetch station information given the station_id
- */
-/*$http.get(url + value).success(
- function(data) {
- success(data);
- })
- .error(function(error) {
- ///alert(error);
- $scope.data.error = error;
- });
- *//*
- }
- });
- }
- $scope.payment = {};
- $scope.formTitle = "Report Offence";
- dhisConfigs.onLoad = function(){
- //Is there a request in the route parameters
- */
-/*if($routeParams.request){
- //There is a request in the route parameters
- $scope.formTitle = "Offence Details";
- $scope.isreadonly = true;
-
-
- var offenceModal = new iroad2.data.Modal("Offence Event",[new iroad2.data.Relation("Offence Registry","Offence")]);
- offenceModal.find($routeParams.id,function(result){
- console.log(JSON.stringify(result));
- $scope.offence = result;
- for(i = 0;i < result.Offences.length;i++){
- $scope.offenceEvents.push(result.Offences[i].Offence_Registry);
- }
- $scope.$apply();
- });
-
- //$scope.$apply();
- if($routeParams.request == "view" || $routeParams.request == "edit")// if the route is /view or /edit
- {
-
- }
- }else{*//*
-
- $scope.$watch("offence.Driver['Driver License Number']", function (value, oldValue) {
- if(value != '')//If the changed value is not empty
- {
- var driver = new iroad2.data.Modal("Driver",[]);
- driver.get({value:$scope.offence.Driver['Driver License Number']},function(result){
- if(result.length == 1)
- {
- $scope.offence.Driver = result[0];
-
- }else{
- $scope.offence.Driver.id = undefined;
- }
- $scope.$apply();
- });
- }
- });
- $scope.$watch("offence.Vehicle['Vehicle Plate Number']", function (value, oldValue) {
- if(value != '')//If the changed value is not empty
- {
- var vehicle = new iroad2.data.Modal("Vehicle",[]);
- vehicle.get({value:$scope.offence.Vehicle['Vehicle Plate Number']},function(result){
- if(result.length == 1)
- {
- $scope.offence.Vehicle = result[0];
- }else{
- $scope.offence.Vehicle.id = undefined;
- }
- $scope.$apply();
- });
- }
- });
- $scope.$watch("offence.Police['Rank Number']", function (value, oldValue) {
- if(value != '')//If the changed value is not empty
- {
- var police = new iroad2.data.Modal("Police",[]);
- police.get({value:$scope.offence.Police['Rank Number']},function(result){
- if(result.length == 1)
- {
- $scope.offence.Police = result[0];
- }else{
- $scope.offence.Police.id = undefined;
- }
- $scope.$apply();
- });
- }
- });
- //}
- }
- iroad2.Init(dhisConfigs);
- */
-/**
- *
- * Sets payment
- *//*
-
- $scope.setPayment = function(receipt){
- //Set paid if payment is made
- $scope.offence.paid = (receipt.receipt_number != undefined);
- if($scope.offence.paid){
- $scope.payment = receipt;
-
- }
- }
- */
-/**
- *
- * Checks if the form is readonly
- *//*
-
- $scope.getWriteAccess = function(){
- if($scope.isreadonly){
- return "readonly";
- }
- }
- $scope.data = {};
-
- //Initialize Station  mirrors the one on the database
- $scope.station = {};
-
- //Watch/wait for changes on the station id
- $scope.watchAndFetch('police.station_id',"/api/station/",function(station) {
- //Set Station
- $scope.station = station;
- });
-
- $scope.offenceAdded = false;
- //submit the offence object to the server
- $scope.submitOffence = function() {
-
- $scope.offence.Offences = [];
- for(i = 0; i < $scope.offenceEvents.length;i++)
- {
- $scope.offence.Offences.push({Offence_Registry: $scope.offenceEvents[i]});
- }
-
- var offenceEvent = new iroad2.data.Modal("Offence Event",[]);
- var otherData = {orgUnit:"ij7JMOFbePH",status: "COMPLETED",storedBy: "admin",eventDate:$scope.offence['Offence Date']};
- offenceEvent.save($scope.offence,otherData,function(result){
- console.log("Offence Save successfully:"+JSON.stringify(result));
- var offence = new iroad2.data.Modal("Offence",[]);
- var offences = [];
- for(i = 0; i < $scope.offenceEvents.length;i++)
- {
- offences.push({Offence_Event:result.importSummaries[0].reference,Offence_Registry: $scope.offenceEvents[i].id});
- }
- console.log(JSON.stringify(offences));
- offence.save(offences,otherData,function(result){
- alert("Offence save su")
- console.log("Result:"+JSON.stringify(result));
- },function(result){
- console.log("Error:"+JSON.stringify(result));
- });
- },function(result){
- console.log("Error:"+JSON.stringify(result));
- });
- }
-
-
- */
-/**
- * Get a Yes or No value from a boolean value
- *
- * @param boolean value
- *
- *//*
-
- $scope.getAnswerValue = function(value) {
- if(value)
- {
- return "Yes";
- }else
- {
- return "No";
- }
- };
- */
-/**
- * Get a Block or None value from a boolean value
- *
- * @param boolean value
- *
- *//*
-
- $scope.getDisplayValue = function(value) {
- if(value)
- {
- return "inline-block";
- }else
- {
- return "none";
- }
- };
- $scope.isExpired = function(date1) {
- var date2 = new Date();
- var timestamp=Date.parse(date1)
- if (isNaN(timestamp)==false)
- {
- var d=new Date(timestamp);
- return ((date2.getDate() - d.getDate()) <= 0);
- }
- return false;
- };
- */
-/**
- * Get a Block or None value from a boolean value
- *
- * @param boolean value
- *
- *//*
-
- $scope.showDetails = function(id,e) {
- var elem = e.currentTarget;
- var display = document.getElementById(id).style.display;
- if(display == 'none'){
- document.getElementById(id).style.display = 'inline-block';
- elem.innerHTML = 'Hide Details';
- }else{
- document.getElementById(id).style.display = 'none';
- elem.innerHTML = 'Show Details';
- }
- };
- });*/
