723527044db80e66b389aefbd63144b1cb5bad75
Joseph chingalo <profschingalo@gmail.com>
2015-09-02 16:05:47 +0300

    add basic info and accident witnesses on pf90 form

--- accident-capture/controllers/aggregateAccidentFormController.js
+++ accident-capture/controllers/aggregateAccidentFormController.js
@@ -7,7 +7,7 @@ aggregateAccidentFormApp.controller('aggregateAccidentFormController',function($
     //taking registration number of a given accident :
     $scope.data = {};
     $scope.accidentStatus = false;
-
+    $scope.accidentRegNumber = "";
     //loading necessary files for library
     dhisConfigs.onLoad = function(){
        console.log('success loading library');
@@ -19,17 +19,32 @@ aggregateAccidentFormApp.controller('aggregateAccidentFormController',function($
     $scope.searchAccident = function(){
         if($scope.data['accidentRegistrationNumber']){
             var regNumber = $scope.data['accidentRegistrationNumber'];
+            $scope.accidentRegNumber = regNumber;
             $scope.accidentStatus = true;
+            //fetching accident
             var accidentModal = new iroad2.data.Modal('Accident',[]);
-
+            $scope.acccident = null;
             accidentModal.get(new iroad2.data.SearchCriteria('Accident Registration Number',"=",regNumber),function(result){
                console.log('Loading accident');
-                //chsking if result have been found
+                //checking if result have been found
                 if(result.length > 0){
-                    $scope.acccident = result[0];
-                    $scope.$apply();
-                }
+                    if($scope.acccident == result[0]){
+                        console.log('accident found');
+                    }
+                    else{
+                        $scope.acccident = result[0];
+                        var accidentId = result[0].id;
+                        $scope.$apply();
+                        //fetching accident witness
+                        $scope.accidentWitnesses = null;
+                        var accidentWitness = new iroad2.data.Modal('Accident Witness',[]);
+                        accidentWitness.get(new iroad2.data.SearchCriteria('Program_Accident',"=",accidentId),function(results){
 
+                            $scope.accidentWitnesses = results;
+                            $scope.$apply();
+                        });
+                    }
+                }
             });
         }
         else{
