1589ed233046144f485ae979cf2d213c1185aadf
Joseph chingalo <profschingalo@gmail.com>
2015-09-02 11:09:24 +0300

    add controller for pf90 form

--- accident-capture/controllers/aggregateAccidentFormController.js
+++ accident-capture/controllers/aggregateAccidentFormController.js
@@ -1,3 +1,8 @@
 /**
  * Created by joseph on 9/2/15.
  */
+var aggregateAccidentFormApp = angular.module('aggregateAccidentForm',[]);
+aggregateAccidentFormApp.controller('aggregateAccidentFormController',function($scope,$http){
+
+    $scope.reg = "testing controller";
+});
